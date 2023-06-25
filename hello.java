0f5e09a691c9c8757da2cf1be19785b203264c29
gvsiva2008 <95896898+gvsiva2008@users.noreply.github.com>
2023-06-25 21:48:47 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, siva prasad!");
-        System.out.println("Hello, staragile!");
+        System.out.println("Hello, staragile_banglore!");
     }
 }
